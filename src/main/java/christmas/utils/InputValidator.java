e40f09117f09423432da724504adf5d110c1cbbb
isyoo1213 <moethns@gmail.com>
2023-11-10 18:30:37 +0900

    feat: InputValidator 날짜 입력값 숫자형 변환 추가 utils/InputValidator - convertInputToDate() 추가   - castingStringToInteger()     - isNonNumeric() docs/README.md - 기능 명세 구현 체크

--- src/main/java/christmas/utils/InputValidator.java
+++ src/main/java/christmas/utils/InputValidator.java
@@ -15,6 +15,23 @@ public class InputValidator {
         return userInput;
     }
 
+    public Integer convertInputToDate(String preprocessedInput) {
+        return castStringToInteger(preprocessedInput);
+    }
+
+    private Integer castStringToInteger(String preprocessedInput) {
+        isNonNumeric(preprocessedInput);
+        return Integer.parseInt(preprocessedInput);
+    }
+
+    private void isNonNumeric(String preprocessedInput) {
+        try {
+            Integer.parseInt(preprocessedInput);
+        } catch (NumberFormatException e) {
+            ExceptionMessages.NON_NUMERIC_INPUT.throwException();
+        }
+    }
+
     private boolean isNull(String userInput) {
         return userInput == null;
     }
