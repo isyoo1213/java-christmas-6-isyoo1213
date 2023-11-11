7648ca79a36d6229cebe252e0da083326b930d78
isyoo1213 <moethns@gmail.com>
2023-11-11 19:40:16 +0900

    feat: ExceptionMessages 오류 추가 및 이름 변경 constants/ExceptionMessages - NON_EXISTING_NEMU 추가 - WRONG_MENU_AMOUNT -> WRONG_EACH_MENU_AMOUNT 변경

--- src/main/java/christmas/constants/ExceptionMessages.java
+++ src/main/java/christmas/constants/ExceptionMessages.java
@@ -4,9 +4,10 @@ public enum ExceptionMessages {
     NULL_INPUT("null은 입력할 수 없습니다."),
     EMPTY_INPUT("값을 입력해주세요."),
     NON_NUMERIC_INPUT("숫자를 입력해주세요."),
+    NON_EXISTING_MENU("존재하지 않는 메뉴입니다."),
     WRONG_ORDER_FROMAT_INPUT("메뉴 주문 형식에 맞게 입력해주세요."),
     DUPLICATED_MENU_ORDER("중복되는 메뉴 주문이 존재합니다. 수량을 조정해주세요."),
-    WRONG_MENU_AMOUNT("각 메뉴는 1개 이상을 주문해주세요."),
+    WRONG_EACH_MENU_AMOUNT("각 메뉴는 1개 이상을 주문해주세요."),
     WRONG_MENU_TOTAL_AMOUNT("총 메뉴 주문 수량이 20개 이하여야 이벤트를 참여할 수 있습니다."),
     WRONG_DAY_NUMBER_RANGE("날짜는 1 ~ 31 사이의 숫자를 입력해주세요."),
     ONLY_BEVERAGE_ORDER("음료만 주문할 경우 이벤트를 참여할 수 없습니다.");
