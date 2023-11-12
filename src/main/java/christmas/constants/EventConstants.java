d64d4702b938df67d61c4d8095b752548990c8b2
isyoo1213 <moethns@gmail.com>
2023-11-12 22:30:20 +0900

    feat: EventConstants 날짜 계산에 필요한 상수 추가 constants/EventConstants

---
+++ src/main/java/christmas/constants/EventConstants.java
@@ -0,0 +1,8 @@
+package christmas.constants;
+
+public class EventConstants {
+    public static final Integer DAYS_OF_WEEK = 7;
+    public static final Integer ZERO = 0;
+    public static final Integer END_DAY_OF_MONTH = 31;
+    public static final Integer CHRISTMAS_DATE = 25;
+}
