7b2b0c689e3cc65664b50fa803f9627104fe18bb
isyoo1213 <moethns@gmail.com>
2023-11-13 22:08:28 +0900

    feat: Days 날짜입력값 enum 상수화 기능 추가 constants/Days - calculateDate() 추가   - hasDividedDate() docs/README.md - constants   - 미구현 기능 명세 추가 및 체크

--- src/main/java/christmas/constants/Days.java
+++ src/main/java/christmas/constants/Days.java
@@ -2,6 +2,7 @@ package christmas.constants;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public enum Days {
     WEEKDAY("평일", Arrays.asList(0, 4, 5, 6)),
@@ -17,5 +18,20 @@ public enum Days {
     Days(String category, List<Integer> dividedDays) {
         this.category = category;
         this.dividedDays = dividedDays;
+    }
+
+    public static Days calculateDate(Integer date) {
+        if (Objects.equals(date, EventConstants.CHRISTMAS_DATE)) {
+            return SPECIALDAY;
+        }
+        int dividedDate = date % EventConstants.DAYS_OF_WEEK;
+        return Arrays.stream(Days.values())
+                .filter((dividedDays) -> dividedDays.hasDividedDate(dividedDate))
+                .findAny()
+                .orElse(UNCATEGORIZED);
+    }
+
+    private boolean hasDividedDate(Integer dividedDate) {
+        return dividedDays.stream().anyMatch(dividedDays -> dividedDays.equals(dividedDate));
     }
 }
