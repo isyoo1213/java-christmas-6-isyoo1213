50bb812eb101ddb3a298142b7a88a97230b7ae46
isyoo1213 <moethns@gmail.com>
2023-11-13 23:50:43 +0900

    feat: EventConstants 전역으로 사용할 상수 추가 constants/EventConstants - MINIMUM_PARTICIPATION_AMOUNT - GIFT_PRESENTATION_EVENT_AMOUNT - BASIC_DDAY_DISCOUNT_AMOUNT - DDAY_DISCOUNT_UNIT_AMOUNT - WEEKDAY_DISCOUNT_AMOUNT - WEEKEND_DISCOUNT_AMOUNT - SPECIALDAY_DISCOUNT_AMOUNT - GIFT_PRESENTATION_BENEFIT

--- src/main/java/christmas/constants/EventConstants.java
+++ src/main/java/christmas/constants/EventConstants.java
@@ -5,4 +5,12 @@ public class EventConstants {
     public static final Integer ZERO = 0;
     public static final Integer END_DAY_OF_MONTH = 31;
     public static final Integer CHRISTMAS_DATE = 25;
+    public static final Integer MINIMUM_PARTICIPATION_AMOUNT = 10000;
+    public static final Integer GIFT_PRESENTATION_EVENT_AMOUNT = 120000;
+    public static final Integer BASIC_DDAY_DISCOUNT_AMOUNT = 1000;
+    public static final Integer DDAY_DISCOUNT_UNIT_AMOUNT = 100;
+    public static final Integer WEEKDAY_DISCOUNT_AMOUNT = 2023;
+    public static final Integer WEEKEND_DISCOUNT_AMOUNT = 2023;
+    public static final Integer SPECIALDAY_DISCOUNT_AMOUNT = 1000;
+    public static final Integer GIFT_PRESENTATION_BENEFIT = 25000;
 }
