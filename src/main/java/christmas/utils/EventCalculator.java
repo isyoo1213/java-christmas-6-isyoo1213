4437c7bcf8008c0a467acc20c44d4c7cfecdb579
isyoo1213 <moethns@gmail.com>
2023-11-14 00:14:53 +0900

    feat: EventCalculator 이벤트 참여여부 반환 기능 utils/EventCalculator - isParticipate() 추가   - isOnlyBeverage()   - isUnderMinimumAmount() docs/README.md - 기능 명세 구현 체크

---
+++ src/main/java/christmas/utils/EventCalculator.java
@@ -0,0 +1,29 @@
+package christmas.utils;
+
+import christmas.model.Menu;
+
+import java.util.List;
+
+import static christmas.constants.EventConstants.*;
+
+public class EventCalculator {
+    private final String BEVERAGE_CATEGORY_STRING = "음료";
+
+    public boolean isParticipate(Menu menu) {
+        int totalPrice = menu.provideTotalPrice();
+        if (isUnderMinimumAmount(totalPrice) || isOnlyBeverage(menu)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isOnlyBeverage(Menu menu) {
+        List<String> categories = menu.provideMenuCategories();
+        categories.remove(BEVERAGE_CATEGORY_STRING);
+        return categories.size() == 0;
+    }
+
+    private boolean isUnderMinimumAmount(int totalAmount) {
+        return totalAmount < MINIMUM_PARTICIPATION_AMOUNT;
+    }
+}
