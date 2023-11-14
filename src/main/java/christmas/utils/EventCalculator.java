df478d7ce18a16c4807cdb87c74c0fb37d9aac1f
isyoo1213 <moethns@gmail.com>
2023-11-14 17:27:38 +0900

    feat: EventCalculator 이벤트 적용 기능 추가 - calculateEvents() 추가   - calculateGiftPresentation()   - calculateDiscount()     - applySpecialdayDiscount()     - applyWeekendDiscount()     - applyWeekdayDiscount()     - applyXmasDdayDiscount() docs/README.md - 기능 명세 구현 체크

--- src/main/java/christmas/utils/EventCalculator.java
+++ src/main/java/christmas/utils/EventCalculator.java
@@ -1,8 +1,12 @@
 package christmas.utils;
 
+import christmas.constants.Events;
+import christmas.model.Date;
 import christmas.model.Menu;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static christmas.constants.EventConstants.*;
 
@@ -24,5 +28,52 @@ public class EventCalculator {
 
     private boolean isUnderMinimumAmount(int totalAmount) {
         return totalAmount < MINIMUM_PARTICIPATION_AMOUNT;
+    }
+
+    public Map<Events, Integer> calculateEvents(Menu menu, Date date) {
+        Map<Events, Integer> availableEvents = new HashMap<>();
+        calculateDiscount(menu, date, availableEvents);
+        calculateGiftPresentation(menu, availableEvents);
+        return availableEvents;
+    }
+
+    private void calculateGiftPresentation(Menu menu, Map<Events, Integer> availableEvents) {
+        if (menu.provideTotalPrice() >= GIFT_PRESENTATION_EVENT_AMOUNT) {
+            availableEvents.put(Events.GIFT_PRESENTATION, GIFT_PRESENTATION_BENEFIT);
+        }
+    }
+
+    private void calculateDiscount(Menu menu, Date date, Map<Events, Integer> availableEvents) {
+        applyXmasDdayDiscount(date, availableEvents);
+        applyWeekdayDiscount(menu, date, availableEvents);
+        applyWeekendDiscount(menu, date, availableEvents);
+        applySpecialdayDiscount(date, availableEvents);
+    }
+
+    private void applySpecialdayDiscount(Date date, Map<Events, Integer> availableEvents) {
+        if (date.isSpecialday()) {
+            availableEvents.put(Events.SPECIAL_DISCOUNT, SPECIALDAY_DISCOUNT_AMOUNT);
+        }
+    }
+
+    private void applyWeekendDiscount(Menu menu, Date date, Map<Events, Integer> availableEvents) {
+        if (date.isWeekend()) {
+            Integer discountAmount = WEEKEND_DISCOUNT_AMOUNT * menu.provideDessertAmount();
+            availableEvents.put(Events.WEEKEND_DISCOUNT, discountAmount);
+        }
+    }
+
+    private void applyWeekdayDiscount(Menu menu, Date date, Map<Events, Integer> availableEvents) {
+        if (date.isWeekday()) {
+            Integer discountAmount = WEEKDAY_DISCOUNT_AMOUNT * menu.provideDessertAmount();
+            availableEvents.put(Events.WEEKDAY_DISCOUNT, discountAmount);
+        }
+    }
+
+    private void applyXmasDdayDiscount(Date date, Map<Events, Integer> availableEvents) {
+        if (date.isFirstToXmas()) {
+            Integer discountAmount = BASIC_DDAY_DISCOUNT_AMOUNT + (DDAY_DISCOUNT_UNIT_AMOUNT * date.calculateDaysToXmas());
+            availableEvents.put(Events.CHRISTMAS_DDAY_DISCOUNT, discountAmount);
+        }
     }
 }
