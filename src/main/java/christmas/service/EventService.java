ccbc4d8af0f968ba00703008d163ce7126f02878
isyoo1213 <moethns@gmail.com>
2023-11-14 23:03:53 +0900

    feat: EventService 이벤트 계산 기능 추가 service/EventService - calculateEvents() 추가 docs/README.md - 기능 명세 구현 체크

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,13 +1,20 @@
 package christmas.service;
 
+import christmas.constants.EventConstants;
 import christmas.constants.Events;
 import christmas.model.Date;
 import christmas.model.Menu;
 import christmas.model.Order;
+import christmas.utils.EventCalculator;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EventService {
+    EventCalculator eventCalculator = new EventCalculator();
 
     public Order saveOrder(Date visitingDate, Menu orderedMenu, Map<Events, Integer> eventResult) {
         return new Order(visitingDate, orderedMenu, eventResult);
@@ -19,5 +26,12 @@ public class EventService {
 
     public Menu saveOrderedMenu(Map<String, Integer> convertedOrderedMenu) {
         return new Menu(convertedOrderedMenu);
+    }
+
+    public Map<Events, Integer> calculateEvents(Date visitingDate, Menu orderedMenu) {
+        if (!eventCalculator.isParticipate(orderedMenu)) {
+            return new HashMap<>();
+        }
+        return eventCalculator.calculateEvents(orderedMenu, visitingDate);
     }
 }
