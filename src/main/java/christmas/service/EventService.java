fad141276014ff8eb89a594d30a2dad75a998d95
isyoo1213 <moethns@gmail.com>
2023-11-15 02:44:38 +0900

    feat: EventService 증정 이벤트 결과 계산 추가 service/EventService - calculateGiftPresentationMenu 추가 docs/README.md - 기능 명세 구현 체크

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,19 +1,21 @@
 package christmas.service;
 
-import christmas.constants.EventConstants;
 import christmas.constants.Events;
 import christmas.model.Date;
 import christmas.model.Menu;
 import christmas.model.Order;
 import christmas.utils.EventCalculator;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static christmas.constants.EventConstants.*;
+
 public class EventService {
+    public static final Integer GIFT_PRESENTATION_AMOUNT = 1;
+    public static final String GIFT_PRESENTATION_UNIT = "개";
+
     EventCalculator eventCalculator = new EventCalculator();
 
     public Order saveOrder(Date visitingDate, Menu orderedMenu, Map<Events, Integer> eventResult) {
@@ -33,5 +35,14 @@ public class EventService {
             return new HashMap<>();
         }
         return eventCalculator.calculateEvents(orderedMenu, visitingDate);
+    }
+
+    public String calculateGiftPresentationMenu(List<String> eventsResult) {
+        for (String eventResult : eventsResult) {
+            if (eventResult.contains(Events.provideEventName(Events.GIFT_PRESENTATION))) {
+                return Events.provideEventName(Events.GIFT_PRESENTATION) + SPACING_STRING + GIFT_PRESENTATION_AMOUNT + GIFT_PRESENTATION_UNIT;
+            }
+        }
+        return NON_APPLIED_STRING;
     }
 }
