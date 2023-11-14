dd5f27c820e2fe50cca2adfbec66ccbe31d6f35a
isyoo1213 <moethns@gmail.com>
2023-11-14 19:14:27 +0900

    feat: Order 주문 정보와 이벤트적용 정보 클래스 추가 model/Order - 생성자를 통해 방문날짜/주문메뉴/할인전총금액/이벤트적용내역을 문자열로 저장하는   클래스 - calculateEventsResultInfo() 추가

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,38 @@
+package christmas.model;
+
+import christmas.constants.EventConstants;
+import christmas.constants.Events;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Order {
+    private final Map<String, List<String>> orderInfo;
+
+    public Order(Date date, Menu menu, Map<Events, Integer> eventsResult) {
+        Map<String, List<String>> orderInfo = new HashMap<>();
+        List<String> dateInfo = date.provideDateInfo();
+        List<String> orderedMenusInfo = List.copyOf(menu.provideOrderedMenusInfo());
+        List<String> orderedMenusTotalPrice = List.of(new DecimalFormat("###,###").format(menu.provideTotalPrice()));
+        List<String> eventsResultInfo = calculateEventsResultInfo(eventsResult);
+        orderInfo.put("방문 날짜", dateInfo);
+        orderInfo.put("주문 메뉴", orderedMenusInfo);
+        orderInfo.put("할인 전 총주문 금액", orderedMenusTotalPrice);
+        orderInfo.put("혜택 내역", eventsResultInfo);
+
+        this.orderInfo = Map.copyOf(orderInfo);
+    }
+
+    private List<String> calculateEventsResultInfo(Map<Events, Integer> eventsResult) {
+        List<String> eventsResultInfo = new ArrayList<>();
+        if (eventsResult.isEmpty()) {
+            return List.of("없음");
+        }
+        for (Map.Entry<Events, Integer> eventResult : eventsResult.entrySet()) {
+            String eventName = Events.provideEventName(eventResult.getKey());
+            String benefitAmount = new DecimalFormat("###,###").format(eventsResult.get(eventResult));
+            eventsResultInfo.add(eventName + EventConstants.COLONS_STRING+ " " + benefitAmount);
+        }
+        return eventsResultInfo;
+    }
+}
