5aeca26aad5498f0ba96e8212a165fb53618cfa9
isyoo1213 <moethns@gmail.com>
2023-11-15 17:36:14 +0900

    fix: EventService 증정 메뉴 문자열 오류 수정 증정 메뉴 이벤트 계산시, 증정 메뉴의 이름이 아닌 증정 이벤트의 이름이 출력되는 오류 수정 service/EventService - calculateGiftPresentationMenu()   - 증정 이벤트 이름이 아닌 증정 메뉴 이름의 상수로 변경 - GIFT_PRESENTATION_MENU 추가

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -18,6 +18,7 @@ public class EventService {
     public static final Integer TREE_BADGE_AMOUNT = 10000;
     public static final Integer SANTA_BADGE_AMOUNT = 20000;
     public static final Integer GIFT_PRESENTATION_AMOUNT = 1;
+    public static final String GIFT_PRESENTATION_MENU = "샴페인";
     public static final String GIFT_PRESENTATION_UNIT = "개";
 
     EventCalculator eventCalculator = new EventCalculator();
@@ -44,7 +45,7 @@ public class EventService {
     public String calculateGiftPresentationMenu(List<String> eventsResult) {
         for (String eventResult : eventsResult) {
             if (eventResult.contains(Events.provideEventName(Events.GIFT_PRESENTATION))) {
-                return Events.provideEventName(Events.GIFT_PRESENTATION) + SPACING_STRING + GIFT_PRESENTATION_AMOUNT + GIFT_PRESENTATION_UNIT;
+                return GIFT_PRESENTATION_MENU + SPACING_STRING + GIFT_PRESENTATION_AMOUNT + GIFT_PRESENTATION_UNIT;
             }
         }
         return NON_APPLIED_STRING;
