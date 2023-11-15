7722760bef2e560dda2c11542e10ad2d3d3853d6
isyoo1213 <moethns@gmail.com>
2023-11-15 16:37:06 +0900

    test: EventCalculatorTest 누락된 이벤트 테스트 추가 EventCalculator를 의존하는 EventService에서만 테스트가 진행되어, 누락된 calculateEvents() 기능에 대한 테스트 추가 utils/EventCalculatorTest - calculateEventsNonGiftTest()   - 이벤트 계산 테스트 - 증정 이벤트 미참여 데이터 - 성공 - calculateEventNonDdayTest()   - 이벤트 계산 테스트 - 크리스마스 디데이 할인 미참여 데이터 - 성공 - calculateEventNonSpcialTest()   - 이벤트 계산 테스트 - 특별 할인 미참여 데이터 - 성공 - calculateEventWithGiftTest()   - 이벤트 계산 테스트 - 증정 이벤트 참여 데이터 - 성공

--- src/test/java/christmas/utils/EventCalculatorTest.java
+++ src/test/java/christmas/utils/EventCalculatorTest.java
@@ -1,11 +1,14 @@
 package christmas.utils;
 
+import christmas.constants.Events;
+import christmas.model.Date;
 import christmas.model.Menu;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
@@ -36,5 +39,71 @@ public class EventCalculatorTest {
         Menu testMenus = new Menu(testMenuOrder);
         boolean expectedResult = false;
         assertThat(eventCalculator.isParticipate(testMenus)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("이벤트 계산 테스트 - 증정 이벤트 미참여 데이터 - 성공")
+    @Test
+    void calculateEventsNonGiftTest() {
+        Map<String, Integer> testMenuOrder = new LinkedHashMap<>();
+        testMenuOrder.put("타파스", 2);
+        testMenuOrder.put("양송이수프", 2);
+        testMenuOrder.put("초코케이크", 3);
+        Menu testMenus = new Menu(testMenuOrder);
+        Date testDate = new Date(25);
+
+        Map<Events, Integer> expectedResult = new LinkedHashMap<>();
+        expectedResult.put(Events.CHRISTMAS_DDAY_DISCOUNT, 3400);
+        expectedResult.put(Events.WEEKDAY_DISCOUNT, 6069);
+        expectedResult.put(Events.SPECIAL_DISCOUNT, 1000);
+        assertThat(eventCalculator.calculateEvents(testMenus, testDate)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("이벤트 계산 테스트 - 크리스마스 디데이 할인 미참여 데이터 - 성공")
+    @Test
+    void calculateEventNonDdayTest() {
+        Map<String, Integer> testMenuOrder = new LinkedHashMap<>();
+        testMenuOrder.put("타파스", 2);
+        testMenuOrder.put("양송이수프", 2);
+        testMenuOrder.put("초코케이크", 3);
+        Menu testMenus = new Menu(testMenuOrder);
+        Date testDate = new Date(26);
+
+        Map<Events, Integer> expectedResult = new LinkedHashMap<>();
+        expectedResult.put(Events.WEEKDAY_DISCOUNT, 6069);
+        assertThat(eventCalculator.calculateEvents(testMenus, testDate)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("이벤트 계산 테스트 - 특별 할인 미참여 데이터 - 성공")
+    @Test
+    void calculateEventNonSpcialTest() {
+        Map<String, Integer> testMenuOrder = new LinkedHashMap<>();
+        testMenuOrder.put("타파스", 2);
+        testMenuOrder.put("양송이수프", 2);
+        testMenuOrder.put("초코케이크", 3);
+        Menu testMenus = new Menu(testMenuOrder);
+        Date testDate = new Date(18);
+
+        Map<Events, Integer> expectedResult = new LinkedHashMap<>();
+        expectedResult.put(Events.CHRISTMAS_DDAY_DISCOUNT, 2700);
+        expectedResult.put(Events.WEEKDAY_DISCOUNT, 6069);
+        assertThat(eventCalculator.calculateEvents(testMenus, testDate)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("이벤트 계산 테스트 - 증정 이벤트 참여 데이터 - 성공")
+    @Test
+    void calculateEventWithGiftTest() {
+        Map<String, Integer> testMenuOrder = new LinkedHashMap<>();
+        testMenuOrder.put("타파스", 2);
+        testMenuOrder.put("양송이수프", 2);
+        testMenuOrder.put("초코케이크", 3);
+        testMenuOrder.put("티본스테이크", 10);
+        Menu testMenus = new Menu(testMenuOrder);
+        Date testDate = new Date(18);
+
+        Map<Events, Integer> expectedResult = new LinkedHashMap<>();
+        expectedResult.put(Events.CHRISTMAS_DDAY_DISCOUNT, 2700);
+        expectedResult.put(Events.WEEKDAY_DISCOUNT, 6069);
+        expectedResult.put(Events.GIFT_PRESENTATION, 25000);
+        assertThat(eventCalculator.calculateEvents(testMenus, testDate)).isEqualTo(expectedResult);
     }
 }
