d5ee7eaa60486307f3316bb8107dbf7362c1e95a
isyoo1213 <moethns@gmail.com>
2023-11-15 13:07:36 +0900

    feat: OutputView 이벤트 프리뷰 메시지 출력 view/OutputView - printEventPreviewMessage() 추가 docs/README.md - 기능 명세 추가 및 구현 체크

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,15 @@
 package christmas.view;
 
+import static christmas.constants.EventConstants.*;
+
 public class OutputView {
 
     public void printWelcomeMessage() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printEventPreviewMessage(int visitingDate) {
+        System.out.println("12월" + SPACING_STRING + visitingDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
     }
 }
