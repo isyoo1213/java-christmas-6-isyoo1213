741e2c94e6d427b0ed7ba13232a3b85276fe1c26
isyoo1213 <moethns@gmail.com>
2023-11-15 13:13:06 +0900

    feat: OutputView 유저 주문 메뉴 정보 출력 추가 view/OutputView - printUserMenusInfo() 추가   - printOrderedMenus()   - printOrderedMenusTotalAmount() docs/README.md - 기능 명세 구현 체크

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,8 @@
 package christmas.view;
 
+import java.text.DecimalFormat;
+import java.util.List;
+
 import static christmas.constants.EventConstants.*;
 
 public class OutputView {
@@ -10,6 +13,25 @@ public class OutputView {
 
     public void printEventPreviewMessage(int visitingDate) {
         System.out.println("12월" + SPACING_STRING + visitingDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
+    }
+
+    public void printUserMenusInfo(List<String> orderedMenusInfo, int orderedMenusTotalAmount) {
+        printOrderedMenus(orderedMenusInfo);
+        printOrderedMenusTotalAmount(orderedMenusTotalAmount);
+    }
+
+    private void printOrderedMenus(List<String> orderedMenus) {
+        System.out.println("<주문 메뉴>");
+        for (String orderedMenu : orderedMenus) {
+            System.out.println(orderedMenu);
+        }
+        System.out.println();
+    }
+
+    private void printOrderedMenusTotalAmount(int orderedMenusTotalAmount) {
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(new DecimalFormat("###,###").format(orderedMenusTotalAmount) + PRICE_AMOUNT_UNIT);
         System.out.println();
     }
 }
